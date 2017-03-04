52757cb19f8c1ad6f68a257a2d1ce171374ba1ba
Nathan Scherneck <nathan.scherneck@gmail.com>
2017-03-03 19:15:31 -0800

    install Laravel and basic Vue app

---
+++ front/src/components/authentication/Login.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="form-group">
+						<input 
+							v-model="email"
+							class="form-control"
+							type="email"
+							placeholder="Email">
+					</div>					
+					<div class="form-group">
+						<input 
+							v-model="password"
+							class="form-control"
+							type="password"
+							placeholder="Password">
+					</div>
+					<button class="btn btn-success pull-right">
+					Login
+					</button>
+				</div>
+			</div>
+		</div>
+	</div> <!-- end of row -->
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	
+</style>
